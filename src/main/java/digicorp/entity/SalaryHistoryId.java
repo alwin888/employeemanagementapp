96599f427bdc148646cec2772d1c08dca95752d8
Shao Jie <shaojie.yee@hotmail.com>
2025-12-03 15:13:10 +0800

    javadocs for salaryHistoryId

--- src/main/java/digicorp/entity/SalaryHistoryId.java
+++ src/main/java/digicorp/entity/SalaryHistoryId.java
@@ -1,3 +1,6 @@
+/**
+ * This package contains enities for the employee management
+ */
 package digicorp.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -17,25 +20,59 @@ import java.util.Objects;
 @Embeddable
 public class SalaryHistoryId implements Serializable {
 
+    /**
+     * Employee number: foreign key to employees table.
+     */
     @Column(name = "emp_no")
     @JsonIgnore
     private int empNo;
 
-
+    /**
+     * fromDate
+     */
     @Column(name = "from_date")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     @Temporal(TemporalType.DATE)
     private LocalDate fromDate;
 
+    /**
+     * Default constructor required by JPA.
+     */
     public SalaryHistoryId() {}
+    /**
+     * Creates a composite key
+     * @param empNo the employee number
+     * @param fromDate the from_date
+     */
     public SalaryHistoryId(int empNo, LocalDate fromDate) { this.empNo = empNo; this.fromDate = fromDate; }
 
+    /**
+     * Returns employee number
+     * @return employee number as an int
+     */
     public int getEmpNo() { return empNo; }
+    /**
+     * Sets employee number
+     * @param empNo the employee number to set
+     */
     public void setEmpNo(int empNo) { this.empNo = empNo; }
 
+    /**
+     * Returns from date
+     * @return from date as a LocalDate
+     */
     public LocalDate getFromDate() { return fromDate; }
+    /**
+     * Sets from date
+     * @param fromDate the from date to set
+     */
     public void setFromDate(LocalDate fromDate) { this.fromDate = fromDate; }
 
+    /**
+     * Compares this composite key with another object for equality.
+     * @param o the object to compare with
+     * @return true if both objects have same employee and department numbers
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -44,6 +81,10 @@ public class SalaryHistoryId implements Serializable {
         return empNo == that.empNo && Objects.equals(fromDate, that.fromDate);
     }
 
+    /**
+    * Returns a hash code value for this composite key.
+    * @return a hash code value for this object
+    */
     @Override
     public int hashCode() {
         return Objects.hash(empNo, fromDate);
