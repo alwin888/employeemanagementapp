e8dae2f0c2d073e31f3c924f82f061793207856f
Shao Jie <shaojie.yee@hotmail.com>
2025-12-03 15:06:37 +0800

    javadoc for DeptEmployeeId

--- src/main/java/digicorp/entity/DeptEmployeeId.java
+++ src/main/java/digicorp/entity/DeptEmployeeId.java
@@ -1,3 +1,6 @@
+/**
+ * This package contains enities for the employee management
+ */
 package digicorp.entity;
 
 
@@ -8,28 +11,63 @@ import java.io.Serializable;
 import java.util.Objects;
 
 /**
- * Composite PK for dept_emp: (emp_no, dept_no)
+ * Composite PK for dept_emp entity: (emp_no, dept_no)
  */
 @Embeddable
 public class DeptEmployeeId implements Serializable {
 
+    /**
+     * Employee number: foreign key to employees table.
+     */
     @Column(name = "emp_no")
     @JsonIgnore
     private int empNo;
 
+    /**
+     * Department number: foreign key to departments table.
+     */
     @Column(name = "dept_no", length = 4)
     @JsonIgnore
     private String deptNo;
 
+    /**
+     * Default constructor required by JPA.
+     */
     public DeptEmployeeId() {}
+    /**
+     * Creates a composite key
+     * @param empNo the employee number
+     * @param deptNo the department number
+     */
     public DeptEmployeeId(int empNo, String deptNo) { this.empNo = empNo; this.deptNo = deptNo; }
 
+    /**
+     * Returns employee number
+     * @return employee number as an int
+     */
     public int getEmpNo() { return empNo; }
+    /**
+     * Sets employee number
+     * @param empNo the employee number to set
+     */
     public void setEmpNo(int empNo) { this.empNo = empNo; }
 
+    /**
+     * Returns department number
+     * @return department number as a String
+     */
     public String getDeptNo() { return deptNo; }
+    /**
+     * Sets department number
+     * @param septNo the department number to set
+     */
     public void setDeptNo(String deptNo) { this.deptNo = deptNo; }
 
+    /**
+     * Compares this composite key with another object for equality.
+     * @param o the object to compare with
+     * @return true if both objects have same employee and department numbers
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -38,6 +76,10 @@ public class DeptEmployeeId implements Serializable {
         return empNo == that.empNo && Objects.equals(deptNo, that.deptNo);
     }
 
+    /**
+    * Returns a hash code value for this composite key.
+    * @return a hash code value for this object
+    */
     @Override
     public int hashCode() {
         return Objects.hash(empNo, deptNo);
